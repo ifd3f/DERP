1e0fb51f7de1189ed5d635f4c3035e30e0fe145c
Astrid Yu <astrid@astrid.tech>
2023-07-25 21:15:02 -0700

    Implement transitive path updates correctly

--- erp/models.py
+++ erp/models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
-from django.db import models
-from computedfields.models import ComputedFieldsModel, computed, compute
+from django.db import models, transaction
+from computedfields.models import (
+    ComputedFieldsModel,
+    computed,
+    precomputed,
+    update_computedfields,
+)
 
 
 MAX_NAME_LENGTH = 64
@@ -46,20 +51,53 @@ class ItemKind(models.Model):
     description = models.TextField()
 
 
-class CostCenter(ComputedFieldsModel):
+class CostCenter(models.Model):
     """
     A cost center for purchases.
     """
 
     name = models.CharField(max_length=MAX_NAME_LENGTH)
     description = models.TextField()
-    parent = models.ForeignKey("CostCenter", null=True, on_delete=models.SET_NULL, related_name='children')
+    parent = models.ForeignKey(
+        "CostCenter", null=True, on_delete=models.SET_NULL, related_name="children"
+    )
 
-    @computed(models.CharField(max_length=128), depends=[("self", ["parent", "id"])])
-    def path(self):
-        """
-        A string of ID's separated by slashes.
+    path = models.CharField(max_length=128, default="")
+    # return str(self.id) if self.parent is None else f"{self.parent.path}/{self.id}"
 
-        This improves efficiency for computing recursive cost centers.
-        """
-        return str(self.id) if self.parent is None else f"{self.parent.id}/{self.id}"
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            # This is our first call. We cannot have any children.
+            # Therefore, save once to generate the ID, then generate the path from the ID.
+            with transaction.atomic():
+                super().save()
+                self.path = (
+                    f"/{self.id}"
+                    if self.parent is None
+                    else f"{self.parent.path}/{self.id}"
+                )
+                super().save(
+                    force_update=True
+                )  # Must be an update to prevent double-insert
+            return
+
+        # Otherwise, we already know the ID and must update child paths.
+        with transaction.atomic():
+            visited = []
+            nodes = [(self, self.parent.path if self.parent else '/')]
+            while nodes:
+                n, parent_path = nodes[-1]
+                nodes.pop()
+
+                n.path = f"{parent_path}/{n.id}"
+                visited.append(n)
+
+                for c in n.children.all():
+                    nodes.append((c, n.path))
+
+            for n in visited:
+                print(n, n.path)
+                models.Model.save(n, force_update=True)
+
+    def __repr__(self):
+        return f"Cost Center {self.name} ({self.id})"
