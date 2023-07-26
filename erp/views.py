673375426dd5fb4efd1ddd4083f0894f5615e13e
Astrid Yu <astrid@astrid.tech>
2023-07-25 21:14:37 -0700

    Separate transaction query into a function

--- erp/views.py
+++ erp/views.py
@@ -19,7 +19,11 @@ class TransactionRow(NamedTuple):
 
 def home(request: HttpRequest):
     root_cost_centers = CostCenter.objects.filter(parent__isnull=True)
-    return render(request, "home.html", {"page_title": "Home", "root_cost_centers": root_cost_centers})
+    return render(
+        request,
+        "home.html",
+        {"page_title": "Home", "root_cost_centers": root_cost_centers},
+    )
 
 
 def cost_center(request: HttpRequest, i: int):
@@ -28,14 +32,24 @@ def cost_center(request: HttpRequest, i: int):
     except CostCenter.DoesNotExist:
         raise Http404
 
+    transactions = query_balance_sheet(cc)
+
     return render(
-        request, "cost_center.html", {"page_title": cc.name, "cost_center": cc}
+        request,
+        "cost_center.html",
+        {"page_title": cc.name, "cost_center": cc, "transactions": transactions},
     )
 
 
-def balance_sheet(request: HttpRequest):
-    p = (
-        Purchase.objects.values()
+def query_balance_sheet(root_cost_center: CostCenter):
+    """
+    Given a root cost center, returns the balance sheet for all transactions in
+    that cost center and all of its children.
+    """
+
+    purchases = (
+        Purchase.objects.filter(cost_center__path__startswith=root_cost_center.path)
+        .values()
         .annotate(
             date=F("purchase_date"),
             name=ExpressionWrapper(
@@ -51,8 +65,9 @@ def balance_sheet(request: HttpRequest):
         )
         .values("date", "name", "cost_center", "cost_center_id", "price", "href")
     )
-    f = (
-        Funding.objects.values()
+
+    fundings = (
+        Funding.objects.filter(cost_center__path__startswith=root_cost_center.path)
         .annotate(
             date=F("funding_date"),
             name=F("name"),
@@ -65,11 +80,14 @@ def balance_sheet(request: HttpRequest):
         )
         .values("date", "name", "cost_center", "cost_center_id", "price", "href")
     )
-    qs = p.union(f).order_by("date")
 
+    raw_rows = purchases.union(fundings).order_by("date")
+
+    # Calculate a cumulative sum
     csum = 0
     rows = []
-    for r in qs:
+    for r in raw_rows:
         csum += r["price"]
         rows.append(TransactionRow(**r, balance=csum))
-    return render(request, "balance_sheet.html", {"transactions": rows})
+
+    return rows
